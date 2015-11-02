eeb467ea3df4784a1f322ca199c600caaf4e4d89
bedavis <bedavis@wcboe.org>
2015-11-01 20:04:07 -0500

    Create turtleletters.py

---
+++ turtleletters.py
@@ -0,0 +1,66 @@
+import turtle
+
+def turtleLetter(letter,tur):
+    if letter=="box":
+        tur.setheading(0)
+        tur.forward(40)
+        tur.right(90)
+        tur.forward(60)
+        tur.right(90)
+        tur.forward(40)
+        tur.right(90)
+        tur.forward(60)
+
+	## Matt 
+
+	## Zach
+	
+	## Noah
+	
+	## Joe
+	
+	## Desmond
+	
+	## Addison
+    	
+
+    ## Brian
+
+
+    ## Ryan
+	
+	## Hunter
+  
+        
+        
+    elif letter == "Ax":
+        # code here
+        tur.forward(40)
+		
+    else:
+        turtleLetter("box",tur)
+	
+
+
+
+
+
+
+
+window = turtle.Screen()
+tur = turtle.Turtle()
+tur.speed(1)
+turtleLetter("M",tur)
+turtleLetter("N",tur)
+turtleLetter("O",tur)
+
+turtleLetter("B",tur)
+turtleLetter("E",tur)
+turtleLetter("H",tur)
+turtleLetter("K",tur)
+turtleLetter("N",tur)
+turtleLetter("Q",tur)
+turtleLetter("W",tur)
+
+
+window.exitonclick()
