6a1efe0246c7f3957d369b22095fd168efcd93ee
zhornet <zhornet2@comcast.net>
2015-11-04 10:02:16 -0500

    Update turtleletters.py

--- turtleletters.py
+++ turtleletters.py
@@ -14,7 +14,44 @@ def turtleLetter(letter,tur):
 	## Matt 
 
         
-	## Zach
+	 
+       
+            ## zach
+    lif letter == "D":
+       
+            
+        tur.pu()
+        tur.forward(10)
+        tur.right(10)
+        tur.pd()
+        
+        tur.right(90)
+        tur.forward(100)
+        tur.left(90)
+        tur.circle(50,180)
+        
+    elif letter == "E":
+
+
+        tur.pu()
+        tur.forward(20)
+        tur.pd()
+
+        tur.forward(30)
+        tur.right(80)
+        tur.forward(20)
+        tur.right(20)
+        tur.forward(30)
+
+    elif letter == "F"
+        tur.pu()
+        tur.forward(20)
+        tur.pd()
+
+        tur.forward(30)
+        tur.right(80)
+        tur.forward(20)
+        tur.right(20)
 	
 	## Noah
 	
